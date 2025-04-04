4cd0cdd9600996de126d8fc982053da26a6bbf03
choicoco92 <kfccoco0926@gmail.com>
2025-04-04 16:03:11 +0900

    ㅅㄷㅁ

---
+++ config.py
@@ -0,0 +1,15 @@
+# config.py
+import os
+import openai
+
+# OpenAI API Key
+OPENAI_API_KEY = os.environ['OPENAI_API_KEY']
+
+# WordPress API settings
+WP_URL = os.environ['WP_URL']
+WP_USERNAME = os.environ['WP_USERNAME']
+WP_PASSWORD = os.environ['WP_PASSWORD']
+
+# 쿠팡 세팅
+CP_ACCESS_KEY = os.environ['CP_ACCESS_KEY']
+CP_SECRET_KEY = os.environ['CP_SECRET_KEY']
