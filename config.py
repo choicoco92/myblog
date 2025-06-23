faf5f14c7c3e798773e3aebdd51241b2bf343577
choicoco92 <kfccoco0926@gmail.com>
2025-06-23 15:54:40 +0900

    Update config.py

--- config.py
+++ config.py
@@ -10,6 +10,11 @@ WP_URL = os.environ['WP_URL']
 WP_USERNAME = os.environ['WP_USERNAME']
 WP_PASSWORD = os.environ['WP_PASSWORD']
 
+# WordPress API settings
+WP_URL2 = os.environ['WP_URL2']
+WP_USERNAME2 = os.environ['WP_USERNAME2']
+WP_PASSWORD2 = os.environ['WP_PASSWORD2']
+
 # 쿠팡 세팅
 CP_ACCESS_KEY = os.environ['CP_ACCESS_KEY']
 CP_SECRET_KEY = os.environ['CP_SECRET_KEY']
